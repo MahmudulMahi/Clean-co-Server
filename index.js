080bc8720490ce48a56628655fe2f48031e2a941
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-06-27 21:55:13 +0600

    update  indevidul  services

--- index.js
+++ index.js
@@ -76,6 +76,20 @@ async function run() {
       res.send(result);
     });
 
+    // UPDATE SERVICE
+    app.patch('/api/v1/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedService = req.body;
+
+      const updateDoc = {
+        $set: {
+          ...updatedService,
+        },
+      };
+      const result = await serviceCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
     // DELETE SERVICE
     app.delete('api/v1/services/:id', async (req, res) => {
